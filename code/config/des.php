8c1c2709b9dfc2cffa48a44df100d92cc545bab7
Ismael Moral <jastertdc@gmail.com>
2019-08-06 22:40:45 +0200

    develop
    Initial commit

---
+++ code/config/des.php
@@ -0,0 +1,10 @@
+<?php
+
+$configPro = require_once __DIR__ . '/pro.php';
+
+return array_merge(
+    $configPro,
+    [
+
+    ]
+);
---
+++ code/config/pro.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'ComponentWriterDatabase' => [
+        'engine'    => 'mysql',
+        'hostname'  => 'sql',
+        'username'  => 'root',
+        'password'  => 'root',
+        'database'  => 'components',
+        'charset'   => 'utf8',
+        'port'      => 3306
+    ],
+    'ComponentReaderDatabase' => [
+        'engine'    => 'mysql',
+        'hostname'  => 'sql',
+        'username'  => 'root',
+        'password'  => 'root',
+        'database'  => 'components',
+        'charset'   => 'utf8',
+        'port'      => 3306
+    ]
+];
